2eec8899cd3da3944c21ef2f124d881d62def503
Mykola Vasyl'yev <mykovasyl@gmail.com>
2021-11-07 21:13:54 -0500

    commit tree 9985c7391555f4e48b6adf7d3b85a99dce34d2f0

---
+++ index.js
@@ -0,0 +1,7 @@
+const divide = function() {
+    return 2000 / 100;
+}
+
+const square = thingToMultiply => thingToMultiply**2;
+
+const add = (thingToAdd, thingToAddToo) => thingToAdd + thingToAddToo;
